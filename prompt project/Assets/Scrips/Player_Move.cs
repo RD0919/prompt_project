6bcf306bc6134b18f2c86924186709654833f295
RD <124419677+RD0919@users.noreply.github.com>
2025-07-04 00:28:11 +0900

    prompt day3
    map 3(복도2), 4(교무실), 5(2-3 교실), 6(운동장) add
    add 4 door to move

--- prompt project/Assets/Scrips/Player_Move.cs
+++ prompt project/Assets/Scrips/Player_Move.cs
@@ -10,17 +10,41 @@ public class Player_Move : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //교실 -> 복도
+        //장소 : 교실1
+
+        //교실 -> 복도1
         if (collision.gameObject.name == "Door (1)")
         {
-            transform.position = new Vector3(12f, 3f, 0f);
+            transform.position = new Vector3(13f, 0f, 0f);
             mainCamera.transform.position = new Vector3(20f, 0f, -10f);
         }
-        //복도 -> 교실
+        //장소 : 복도1
+
+        //복도1 -> 교실
         else if (collision.gameObject.name == "Door (2)")
         {
-            transform.position = new Vector3(7.5f, 3f, 0f);
+            transform.position = new Vector3(7f, 2f, 0f);
             mainCamera.transform.position = new Vector3(0f, 0f, -10f);
+        }
+        //복도1 -> 복도2
+        else if (collision.gameObject.name == "Door (3)")
+        {
+            transform.position = new Vector3(33f, 2f, 0f);
+            mainCamera.transform.position = new Vector3(40f, 0f, -10f);
+        }
+        //복도1 -> 교무실
+        else if (collision.gameObject.name == "Door (6)")
+        {
+            transform.position = new Vector3(5f, 18f, 0f);
+            mainCamera.transform.position = new Vector3(0f, 15f, -10f);
+        }
+        //장소 : 복도2
+
+        //복도2 -> 복도1
+        else if (collision.gameObject.name == "Door (4)")
+        {
+            transform.position = new Vector3(27f, 0f, 0f);
+            mainCamera.transform.position = new Vector3(20f, 0f, -10f);
         }
     }
 
@@ -33,7 +57,9 @@ public class Player_Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //플레이어 x 이동
         float xInput = Input.GetAxisRaw("Horizontal");
+        //플레이어 y 이동
         float yInput = Input.GetAxisRaw("Vertical");
         Vector3 Move = new Vector3(xInput, yInput, 0f);
         transform.position += Move * moveSpeed * Time.deltaTime;
